84c85e7edbfa00f94565c2750d0562123a00aaee
jimmy <jimbotela@gmail.com>
2022-08-25 20:19:29 +0300

    Created & Styled MainNav component , configured tailwind file

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,14 +6,14 @@ module.exports = {
   extends: [
     "plugin:vue/vue3-recommended",
     "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
+    "prettier"],
   parserOptions: {
     parser: "@babel/eslint-parser",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    // "prettier/prettier": ["error", { endOfLine: "auto" }],
   },
   overrides: [
     {
